912205a10081808495510618a5e698b701c1a81a
Daniel Nachun <daniel.nachun@gmail.com>
2024-12-02 22:14:10 -0800

    enable rattler-build

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,22 +2,23 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-mkdir -p ${PREFIX}/libexec/${PKG_NAME}
+mkdir -p ${PREFIX}/libexec/textidote
 mkdir -p ${PREFIX}/bin
 
 # Build and install jar
 ant download-deps
 ant -noinput -buildfile build.xml -Dbuild.targetjdk=19
 
-install -m 644 textidote.jar ${PREFIX}/libexec/${PKG_NAME}
+install -m 644 textidote.jar ${PREFIX}/libexec/textidote
 
 # Create bash and batch wrappers
-tee ${PREFIX}/bin/${PKG_NAME} << EOF
+tee ${PREFIX}/bin/textidote << EOF
 #!/bin/sh
 exec \${JAVA_HOME}/bin/java -jar \${CONDA_PREFIX}/libexec/textidote/textidote.jar "\$@"
 EOF
+chmod +x ${PREFIX}/bin/textidote
 
-tee ${PREFIX}/bin/${PKG_NAME}.cmd << EOF
+tee ${PREFIX}/bin/textidote.cmd << EOF
 call %JAVA_HOME%\bin\java -jar %CONDA_PREFIX%\libexec\textidote\textidote.jar %*
 EOF
 
